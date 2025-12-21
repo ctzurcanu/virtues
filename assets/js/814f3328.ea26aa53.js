357a177d8ccd9c58a38a0d7d6e347fa22b304041
Christian Tzurcanu <ctzurcanu@yahoo.com>
2025-12-21 18:59:04 +0100

    Deploy website - based on dd2ee9fad068eda353dc3ddf04f6a1b26b8014a2

---
+++ assets/js/814f3328.ea26aa53.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvirtues=self.webpackChunkvirtues||[]).push([[7472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Coming Soon","permalink":"/blog/coming-soon","unlisted":false,"date":"2025-12-21T00:00:00.000Z"}]}')}}]);
